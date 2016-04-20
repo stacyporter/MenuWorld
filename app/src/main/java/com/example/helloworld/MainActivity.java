1d103bb4ab3895b84c79ef12873f795dd609e79a
Stacy Porter <mr.stacy.porter@gmail.com>
2016-04-20 22:28:01 +1000

    no message

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -4,27 +4,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.action_bye) {
-            // TODO: say bye
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+    private TextView greetingView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        greetingView = (TextView) findViewById(R.id.greeting)
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_bye) {
+            greetingView.setText(R.string.bye);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
