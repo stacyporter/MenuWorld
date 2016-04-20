425761b9f4c8572f41c92582e7278b8a9d966651
Stacy Porter <mr.stacy.porter@gmail.com>
2016-04-20 22:29:12 +1000

    duh!

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        greetingView = (TextView) findViewById(R.id.greeting)
+        greetingView = (TextView) findViewById(R.id.greeting);
     }
 
     @Override
